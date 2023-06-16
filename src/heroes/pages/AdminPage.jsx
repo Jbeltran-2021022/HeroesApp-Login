e5098d72a817ede5b6e879dcfc06425cf4a26416
Beltrán <106607127+Jbeltran-2021022@users.noreply.github.com>
2023-06-16 09:18:39 -0600

    versión final
    se corrigieron errores
    
    すべてのバグが修正されました!

---
+++ src/heroes/pages/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const AdminPage = () => {
+  return (
+    <>
+        <h1>when eres ADMIN:</h1>
+        <h2>soy ADMIN xdDDddDddDdD</h2>
+        <h3>moflazo cfffffff</h3>
+        
+    </>
+  )
+}
